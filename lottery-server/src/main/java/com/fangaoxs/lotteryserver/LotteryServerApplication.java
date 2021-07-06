eb321f983561dbf80550938434ff31b95abe1c28
FanGaoXS <954278478@qq.com>
2021-07-06 15:47:11 +0800

    lottery@wqk 创建抽奖平台仓库

---
+++ lottery-server/src/main/java/com/fangaoxs/lotteryserver/LotteryServerApplication.java
@@ -0,0 +1,13 @@
+package com.fangaoxs.lotteryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryServerApplication.class, args);
+    }
+
+}
